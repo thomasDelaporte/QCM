ebf4b9adf525a19698ec0b2f90deff69785f0224
Thomas DELAPORTE <55890466+thomasDelaporte@users.noreply.github.com>
2020-07-15 10:19:16 +0200

    Quizzes index query

--- src/main/java/fr/cesi/qcm/controllers/HomeController.java
+++ src/main/java/fr/cesi/qcm/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package fr.cesi.qcm.controllers;
 
+import fr.cesi.qcm.models.Quiz;
 import fr.cesi.qcm.repositories.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +18,11 @@ public class HomeController {
     QuizRepository quizRepository;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        List<Quiz> quizzes = quizRepository.findAll();
+        model.addAttribute("quizzes", quizzes);
+
         return "index";
     }
 
