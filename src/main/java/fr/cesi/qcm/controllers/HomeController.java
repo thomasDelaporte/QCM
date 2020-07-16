655bab6f9876b5b146c93bfb646389fc6dcf1af5
Pablo <pablo.arrieta@epsi.fr>
2020-07-16 16:09:50 +0200

    more data to play with

--- src/main/java/fr/cesi/qcm/controllers/HomeController.java
+++ src/main/java/fr/cesi/qcm/controllers/HomeController.java
@@ -2,14 +2,17 @@ package fr.cesi.qcm.controllers;
 
 import fr.cesi.qcm.models.Question;
 import fr.cesi.qcm.models.Quiz;
+import fr.cesi.qcm.repositories.AnswerRepository;
 import fr.cesi.qcm.repositories.QuestionRepository;
 import fr.cesi.qcm.repositories.QuizRepository;
+import fr.cesi.qcm.repositories.ScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,7 +23,10 @@ public class HomeController {
     QuizRepository quizRepository;
     @Autowired
     QuestionRepository questionRepository;
-
+    @Autowired
+    AnswerRepository answerRepository;
+    @Autowired
+    ScoreRepository scoreRepository;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -32,21 +38,31 @@ public class HomeController {
     }
 
     @RequestMapping(value= "qcm", method = RequestMethod.POST)
-    public String test(Model model) {
+    public String test(Model model,@RequestParam("pseudo") String pseudo,@RequestParam("qcm") String qcm) {
 
         List<Question> questions = questionRepository.findAll();
         model.addAttribute("questions",questions);
+        model.addAttribute("pseudo", pseudo);
+        model.addAttribute("idThemeQuiz", qcm);
         return "qcm";
     }
 
 
-
+    /* debug purpose */
     @GetMapping("/qcm2")
     public String qcm2(Model model) {
 
         List<Question> questions = questionRepository.findAll();
         model.addAttribute("questions",questions);
         return "qcm";
+    }
+
+    @GetMapping("/score2")
+    public String score2(Model model) {
+
+        List<Question> questions = questionRepository.findAll();
+        model.addAttribute("questions",questions);
+        return "score";
     }
 
 
